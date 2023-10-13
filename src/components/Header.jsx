63101b53213b265c89802b8c79c38e8a9880995d
Akash G Gaonkar <akashggaonkar@gmail.com>
2023-10-13 12:55:14 +0530

    stick header functionality added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import "./Header.css"
 function Header() {
+  document.addEventListener('DOMContentLoaded', function() {
+  window.onscroll = function() {myFunction()};
+  var header = document.getElementById("header");
+  var sticky = header.offsetTop;
+
+  function myFunction() {
+    if (window.pageYOffset > sticky) {
+      header.classList.add("sticky");
+    } else {
+      header.classList.remove("sticky");
+    }
+  }
+})
   return (
-    // <div>
       <header id="header">
-    
     <nav id="nav-bar">
     <div className="Heading">GarbageManager</div>
       <ul>
